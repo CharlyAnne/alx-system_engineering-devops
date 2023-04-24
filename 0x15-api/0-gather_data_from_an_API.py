ece898d981ba6cede175028311ac0297b3f2817b
CharlyAnne <icharlieanne@gmial.com>
2023-04-24 20:30:34 +0000

    data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script returns information about a given employee from an API
+"""
+import requests
+import sys
+from sys import argv
+
+
+if __name__ == '__main__':
+    baseUrl = 'https://jsonplaceholder.typicode.com/users'
+    employeeID = sys.argv[1]
+    url = baseUrl + employeeID
+
+    # Get employee name
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    # Get data on the ToDo of the employee
+    todoUrl = url + '/todos'
+    reponse = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+            .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print('\t {}'.format(task.get('title')))
