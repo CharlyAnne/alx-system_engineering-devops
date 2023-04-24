e66148bd100e942b5b06f44f4973c664ede1024f
CharlyAnne <icharlieanne@gmial.com>
2023-04-24 21:43:45 +0000

    export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script extends to export data in JSON format"""
+
+import sys
+import requests
+import json
+
+
+if __name__ == '__main__':
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    employeeId = sys.argv[1]
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    # records all tasks owned by an employee
+    dictionary = {employeeId: []}
+    for task in tasks:
+        dictionary[employeeId].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": username
+        })
+    # exports in JSON format
+    with open('{}.json'.format(employeeId), 'w') as filename:
+        json.dump(dictionary, filename)
