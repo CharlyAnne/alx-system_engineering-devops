29f2b7e70116766eeae89af20360da2245568f15
CharlyAnne <icharlieanne@gmial.com>
2023-05-09 20:05:09 +0000

    top ten task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,18 +5,17 @@ import requests
 
 def top_ten(subreddit):
     """
-    Function requests reddit api and returns the
-    first 10 subreddit titles
+    Function requests reddit api and returns the first 10 subreddit
+    titles
     """
-    baseUrl = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Google Chrome Version 81.0.4044.129"}
     params = {"limit": 10}
-    
-    response = get(baseUrl, headers=user_agent, params=params)
-    resData = response.json()
 
     try:
-        response = resData.get("data").get("children")
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
+        response = data.get("data").get("children")
 
         for post in response:
             print(post.get("data").get("title"))
