ae933cfd1a4512b47306367ac622e7214d95ee82
CharlyAnne <icharlieanne@gmail.com>
2023-05-09 20:45:28 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,20 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """
-     function queries the Reddit API and returns the number of subscribers
+     function request Reddit API and returns the number of subscribers
      for a given subreddit.
      """
+    baseUrl = "https://www.reddit.com/r/{}.json".format(subreddit)
+    headers = {"User-Agent": "Google Chrome Version 81.0.4044.129"}
+    response = get(baseUrl, headers=headers)
+    data = response.json()
+    resData = data.get("data").get("subscribers")
+
+    if subreddit is None:
+        return 0
+
+    try:
+        return resData
+
+    except Exception:
+        return 0
