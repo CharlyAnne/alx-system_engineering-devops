6465f8639fca5a5d88aaf324c17682cbd6780540
Charly <100117263+CharlyAnne@users.noreply.github.com>
2023-05-10 23:57:09 +0100

    Advanced task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -12,20 +12,20 @@ def count_words(subreddit, word_list, after="", count=[]):
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'user-agent': 'bhalut'})
+                           params={"after": after},
+                           headers={"user-agent": "Mozilla/5.0"},
+                           allow_redirects=False)
 
     if request.status_code == 200:
         data = request.json()
 
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
+        for topic in (data["data"]["children"]):
+            for word in topic["data"]["title"].split():
                 for i in range(len(word_list)):
                     if word_list[i].lower() == word.lower():
                         count[i] += 1
 
-        after = data['data']['after']
+        after = data["data"]["after"]
         if after is None:
             save = []
             for i in range(len(word_list)):
